518861bb8445142e5f7ba249e98ec53c5beeac73
MK <fengmk2@gmail.com>
2025-12-17 21:27:43 +0800

    FIXUP

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from '@voidzero-dev/vite-plus';
 
 export default defineConfig({
-  lint: {
-    plugins: ['typescript', 'unicorn', 'react', 'react-perf', 'oxc', 'import'],
-    categories: {
-      correctness: 'error',
-    },
-    overrides: [{
-        files: ['*.svelte'],
-        rules: {
-          'no-unassigned-vars': 'off',
-        },
-      }],
-  },
-  test: {
-  		projects: ['packages/*'],
-  	},
+	lint: {
+		plugins: ['typescript', 'unicorn', 'react', 'react-perf', 'oxc', 'import'],
+		categories: {
+			correctness: 'error',
+		},
+		overrides: [
+			{
+				files: ['*.svelte'],
+				rules: {
+					'no-unassigned-vars': 'off',
+				},
+			},
+		],
+	},
+	test: {
+		projects: ['packages/*'],
+	},
 });
